a69775909f3a69367b274435c50864357dd1a845
Kunalsk36 <kunalkavathekar@gmail.com>
2026-07-23 11:13:41 +0530

    Find nth Fibonacci number

--- FibonacciSeries.js
+++ FibonacciSeries.js
@@ -19,7 +19,7 @@ printFibonacci();
 // Method 2:
 
 function getFibonacciSeries(n){
-    if(n<0) return [];
+    if(n<=0) return [];
     if(n === 1) return [0];
     let result = [0,1];
     for(let i=2;i<n;i++){
@@ -27,12 +27,28 @@ function getFibonacciSeries(n){
     }
     return result;
 }
-console.log(getFibonacciSeries(20));
+console.log(getFibonacciSeries(3));
 
 
-// Method 3: recurrsion: directly print nth number
+//Find nth Fibonacci number
+// Method 1: recurrsion: directly print nth number
 function getFibS(n){
     if(n<=1){ return n;}
     return getFibS(n-1)+getFibS(n-2);
 }
 console.log(getFibS(20));
+
+// Method 2:
+function getFibS2(num){
+    let num1 = 0;
+    let num2 = 1;
+    if(num<=0) return 0;
+    if(num === 1) return 1;
+    for(let i = 2; i <= num; i++){
+        let temp = num1;
+        num1 = num2;
+        num2 = num2 + temp;
+    }
+    return num2;
+}
+console.log(getFibS2(20));
